87379c54427b56bb2b7803da3b4dd5487e226fd8
burakugras <99265170+burakugras@users.noreply.github.com>
2022-02-22 16:52:54 +0300

    Add files via upload
    The basic use of the dictionary class on .NET Core in C# language is intended in the phoneBook application.
    Application description:
    The number of people to be registered will be entered from the keyboard.
    Name and number will be entered with one space between them.
    When the registration process is completed, the name of the person whose information is requested will be entered. If an unregistered name is entered, "Not found" will be printed.

---
+++ phoneBook/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace phoneBook
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var phoneBook = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                string name = s[0];
+                string number = s[1];
+
+                phoneBook.Add(name, number);
+            }
+
+            string searchName = "";
+            while ((searchName = Console.ReadLine()) != null)
+            {
+                if (phoneBook.ContainsKey(searchName))
+                {
+                    Console.WriteLine(searchName+ " = " + phoneBook[searchName]);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        }
+    }
+}
